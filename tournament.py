4234c869cc1022dcf2a690f25f08040058c543e9
Jakob Thunen <jthunen@dev-10.com>
2022-04-07 15:47:45 -0500

    started on tournament addition/subtraction

--- tournament.py
+++ tournament.py
@@ -20,8 +20,7 @@ def load_menus():
     return menulist
 
 def file_menu():
-    menu_loop = True
-    while menu_loop:
+    while True:
         os.system('cls')
         print(load_menus()[0])
         ui = str(input("!> "))
@@ -33,3 +32,49 @@ def file_menu():
             input("Command not recognized. Press 'Enter' to try again.")
     return "error"
     
+def entry_menu(tdict):
+    working_dict = tdict
+    tsize = len(tdict)
+    slot_prompt = f"Enter desired starting slot (1-%d), or type '0' for first available: % {tsize}"
+    while True:
+        os.system('cls')
+        print(load_menus()[1])
+        ui = str(input("#> "))
+        if ui == "1":
+            invalid_name = True
+            while invalid_name:
+                ui = str(input("Enter name: "))
+                if ui in working_dict.values():
+                    input("That name is already in the tournament. Press 'Enter' to try again.")
+                else:
+                    newname = ui
+                    invalid_name = False
+            invalid_place = True
+            while invalid_place:
+                uin = int(input(slot_prompt))
+                if uin == 0:
+                    cur_val = working_dict[1]
+                    i = 1
+                    while cur_val != None:
+                        i++
+                        cur_val = working_dict[i]
+                    working_dict[i] = newname
+                    invalid_place = False
+                elif uin > tsize or uin < 1:
+                    input("That number is outside the required range. Press 'Enter' to try again.")
+                else:
+                    if working_dict[uin] == None:
+                        working_dict[uin] = newname
+                        invalid_place = False
+                    else:
+                        input("That slot is already taken. Press 'Enter' to try again.")
+            input("Success! The new entrant has been added. Press 'Enter to continue.")
+        elif ui == "2":
+            uin = int(input(f"Starting slot (1-%d): % {tsize}"))
+            ui = input("Participant name: ")
+            if working_dict[uin] == ui:
+                working_dict[uin] = None
+                print(f"Success:\n%s has been removed from slot %d. % {ui, uin}")
+            else:
+                print(f"Failure: %s is not in slot %d. % {ui, uin}")
+            input("Press Enter to continue.")
