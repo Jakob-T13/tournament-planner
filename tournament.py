a35773960020fde6aa8a86e8f6f7ee57b1764102
Jakob Thunen <jthunen@dev-10.com>
2022-04-07 14:05:42 -0500

    added load_menus() and file_menu()

--- tournament.py
+++ tournament.py
@@ -1,3 +1,5 @@
+import os
+
 def check_for_password():
     try:
         pwf = open("password.txt", "rt")
@@ -8,3 +10,26 @@ def check_for_password():
         pwf = open("password.txt", "wt")
         pwf.write(pw)
         pwf.close()
+        
+    return pw
+
+def load_menus():
+    menuf = open("menus.txt", "rt")
+    menutext = menuf.read()
+    menulist = menutext.split(";\n")
+    return menulist
+
+def file_menu():
+    menu_loop = True
+    while menu_loop:
+        os.system('cls')
+        print(load_menus[0])
+        ui = str(input("!> "))
+        if ui == "1":
+            return "new"
+        elif ui == "2":
+            return "open"
+        else:
+            print("Command not recognized. Please try again.")
+    return "error"
+    
